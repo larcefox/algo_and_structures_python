6ad22fa96bf22f0726c83fb6c499f63912d45416
larcefox <larcethefox@gmail.com>
2020-02-03 19:31:47 +0300

    Lesson 5 task is done

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,66 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import namedtuple, defaultdict
+
+
+try:
+    corp_count = int(input('Enter number:\n'))
+except ValueError:
+    print("Data is incorrect!")
+
+
+corps = namedtuple('Corps', 'name q1 q2 q3 q4')
+corp_data = []
+
+
+for i in range(corp_count):
+    try:
+        corp_name = input('Enter corporation name:\n')
+        corp_data.append(corps(corp_name,
+                               q1=int(input('Enter pure income for 1 quarter:\n')),
+                               q2=int(input('Enter pure income for 2 quarter:\n')),
+                               q3=int(input('Enter pure income for 3 quarter:\n')),
+                               q4=int(input('Enter pure income for 4 quarter:\n'))
+                               ))
+    except ValueError:
+        print("Data is incorrect!")
+        exit(1)
+
+corp_aver_income = 0
+
+for value in corp_data:
+    corp_aver_income += value.q1 + value.q2 + value.q3 + value.q4
+else:
+    corp_aver_income /= len(corp_data)
+
+for value in corp_data:
+    print(f'Income of {value.name} corporation higher or equally average income') \
+        if value.q1 + value.q2 + value.q3 + value.q4 >= corp_aver_income else \
+        print(f'Income of {value.name} corporation lower then average income')
+
+
+print(corp_aver_income)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+d = defaultdict(list)
+sort_list = []
+
+for i in range(corp_count):
+    corp_name = input('Enter corporation name:\n')
+
+    try:
+        [d[corp_name].append(int(input(f'Enter pure income for {j + 1} quarter:\n'))) for j in range(4)]
+    except ValueError:
+        print("Data is incorrect!")
+        exit(1)
+    d[corp_name].append(sum(d[corp_name]))
+
+
+avg_income = sum([k[4] for k in list(d.values())]) / corp_count
+
+print(f'Income of {list(filter(lambda x: d[x][4] > avg_income, d))} corporations is higher than average income')
+print(f'Income of {list(filter(lambda x: d[x][4] < avg_income, d))} corporations is lower than average income')
+print(f'Income of {list(filter(lambda x: d[x][4] == avg_income, d))} corporations is equal than average income')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,40 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+from collections import defaultdict
+
+hex_num = d = defaultdict(list)
+for i in range(2):
+    hex_num[i].extend(list(input('Enter number:\n').upper()))
+
+    for simbol in hex_num[i]:
+        if simbol not in '0123456789ABCDEF':
+            print("Data is incorrect!")
+            exit(1)
+
+summ = hex(sum(int(''.join(j), 16) for j in hex_num.values())).upper()
+mull = hex(list(int(''.join(j), 16) for j in hex_num.values())[0] * list(int(''.join(j), 16) for j in hex_num.values())[1])
+
+print(summ, mull)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+class HexSumMul:
+    def __init__(self, hex_num_c):
+        self.hex_num_c = hex_num_c
+
+    def __add__(self, other):
+        return hex(int(''.join(self.hex_num_c), 16) + int(''.join(other.hex_num_c), 16)).upper()
+
+    def __mul__(self, other):
+        return hex(int(''.join(self.hex_num_c), 16) * int(''.join(other.hex_num_c), 16)).upper()
+
+
+hex_num_st = HexSumMul(list(input('Enter number:\n').upper()))
+hex_num_nd = HexSumMul(list(input('Enter number:\n').upper()))
+
+
+hex_sum = hex_num_st + hex_num_nd
+hex_mul = hex_num_st * hex_num_nd
+
+print(hex_sum, hex_mul)
