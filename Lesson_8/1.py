ebe2ffceda2b153cfeac8454c0bd51a2bda4a81d
larcefox <larcethefox@gmail.com>
2020-02-03 19:35:28 +0300

    Lesson 8 task is done

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -1,3 +1,56 @@
 """
 1. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
 """
+from collections import Counter, deque
+
+string = ('of all the things i\'ve lost, i miss my minde the most!')
+#string = "beep boop beer!"
+
+
+def binary_tree(s):
+
+    s = Counter(list(s))
+    s = deque(sorted(s.items(), key=lambda x: x[1]))
+
+    if len(s) != 1:
+        while len(s) > 1:
+            weight = s[0][1] + s[1][1]
+            node = {0: s.popleft()[0],
+                    1: s.popleft()[0]}
+
+
+            for num, item in enumerate(s):
+                if weight > item[1]:
+                    continue
+                else:
+                    s.insert(num, (node, weight))
+                    break
+            else:
+                s.append((node, weight))
+    else:
+        weight = s[0][1]
+        node = {0: s.popleft()[0],
+                1: None}
+        s.append((node, weight))
+
+    return s[0][0]
+
+
+tree = binary_tree(string)
+coding_valuse = {}
+
+
+def haffman_encoding(btree, code=''):
+
+    if not isinstance(btree, dict):
+        coding_valuse[btree] = code
+    else:
+        haffman_encoding(btree[0], code=f'{code}0')
+        haffman_encoding(btree[1], code=f'{code}1')
+
+
+haffman_encoding(tree)
+
+for i in string:
+    print(coding_valuse[i], end=' ')
+print(coding_valuse)
--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,45 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+from collections import Counter
+import hashlib
+
+
+string = list('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') # пocтoяннoe cчacтьe (нем.)
+hash_obj = hashlib.md5()
+
+#example
+'''
+length = len(string)
+
+for start in range(length):
+
+    if start == 0:
+        length = len(string) - 1
+    else:
+        length = len(string)
+
+    for end in range(length, start, -1):
+        print(string[start:end])
+'''
+
+s = 0
+substring = Counter()
+
+for letter_start in range(len(string)):
+
+    if s == 0:
+        e = len(string) - 1
+    else:
+        e = len(string)
+
+    for letter_end in range(e - s):
+
+        hash_obj.update(''.join(string[s:e]).encode())
+        substring[hash_obj.hexdigest()] += 1
+        if len(string[s:e]) == 1:
+            print(string[s:e], hash_obj.hexdigest())
+        e -= 1
+    s += 1
+
+print(substring, len(substring))
